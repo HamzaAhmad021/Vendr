dfa405a401ee80bd36eeb35b31d6a39c60350f30
HamzaAhmad021 <hamzahmadxyz@gmail.com>
2026-07-06 14:18:55 +0500

    HomePage design changes

--- src/Screens/Home/HomeTab/index.js
+++ src/Screens/Home/HomeTab/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import styles from './style';
 
 const HomeTab = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Tab</Text>
-    </View>
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        hidden={false}
+        backgroundColor="#1a6fd8"
+        translucent={true}
+      />
+      <View style={styles.container}>
+        <View>
+          <View style={styles.headerContainer}></View>
+        </View>
+      </View>
+    </>
   );
 };
 export default HomeTab;
--- src/Screens/Home/HomeTab/style.js
+++ src/Screens/Home/HomeTab/style.js
@@ -1,16 +1,25 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { FONTS } from '../../../Assets/Themes';
+
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
   },
   text:{
     fontSize: 25,
     fontFamily: FONTS.Comfortaa_Bold,
     textAlign: 'center',
     marginTop: 20,
+  },
+  headerContainer:{
+    backgroundColor: '#1a6fd8',
+    height: height * 0.3,
+    width: width,
+    borderBottomLeftRadius: 35,
+    borderBottomRightRadius: 35,
   }
 });
 export default styles;
